dabe9f86570d89dfd61ddb9e250ab2160ca435e7
DevExpressExampleBot <fakeEmail>
2020-04-17 18:06:26 +0300

    autogenerate VB

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -7,66 +7,66 @@ Imports DevExpress.XtraRichEdit.API.Native
 Imports DevExpress.Portable
 
 Namespace RichEditLogViewer
-    Partial Public Class Form1
-        Inherits Form
+	Partial Public Class Form1
+		Inherits Form
 
-        Private rnd As New Random()
-        Private mesageFormat As String = "{0}: {1,-20} - {2}"
+		Private rnd As New Random()
+		Private mesageFormat As String = "{0}: {1,-20} - {2}"
 
-        Public Sub New()
-            InitializeComponent()
+		Public Sub New()
+			InitializeComponent()
 
-            richEditControl1.ReadOnly = True
-            richEditControl1.ActiveViewType = RichEditViewType.Draft
-            richEditControl1.Options.HorizontalRuler.Visibility = RichEditRulerVisibility.Hidden
-            richEditControl1.Views.DraftView.BackColor = Color.Black
-            richEditControl1.Views.DraftView.Padding = New PortablePadding(0)
-            richEditControl1.CreateNewDocument()
-        End Sub
+			richEditControl1.ReadOnly = True
+			richEditControl1.ActiveViewType = RichEditViewType.Draft
+			richEditControl1.Options.HorizontalRuler.Visibility = RichEditRulerVisibility.Hidden
+			richEditControl1.Views.DraftView.BackColor = Color.Black
+			richEditControl1.Views.DraftView.Padding = New PortablePadding(0)
+			richEditControl1.CreateNewDocument()
+		End Sub
 
-        Private Sub richEditControl1_DocumentLoaded(ByVal sender As Object, ByVal e As EventArgs) Handles richEditControl1.DocumentLoaded
-            ApplyDefaultDocumentFormat()
-        End Sub
+		Private Sub richEditControl1_DocumentLoaded(ByVal sender As Object, ByVal e As EventArgs) Handles richEditControl1.DocumentLoaded
+			ApplyDefaultDocumentFormat()
+		End Sub
 
-        Private Sub richEditControl1_EmptyDocumentCreated(ByVal sender As Object, ByVal e As EventArgs) Handles richEditControl1.EmptyDocumentCreated
-            ApplyDefaultDocumentFormat()
-        End Sub
+		Private Sub richEditControl1_EmptyDocumentCreated(ByVal sender As Object, ByVal e As EventArgs) Handles richEditControl1.EmptyDocumentCreated
+			ApplyDefaultDocumentFormat()
+		End Sub
 
-        Private Sub timer1_Tick(ByVal sender As Object, ByVal e As EventArgs) Handles timer1.Tick
-            Dim document As Document = richEditControl1.Document
-            Dim prefix As String = ControlChars.CrLf & "New message"
+		Private Sub timer1_Tick(ByVal sender As Object, ByVal e As EventArgs) Handles timer1.Tick
+			Dim document As Document = richEditControl1.Document
+			Dim prefix As String = vbCrLf & "New message"
 
-            If document.Paragraphs(0).Range.Length <= 1 Then
-                prefix = prefix.TrimStart()
-            End If
+			If document.Paragraphs(0).Range.Length <= 1 Then
+				prefix = prefix.TrimStart()
+			End If
 
-            document.AppendText(String.Format(mesageFormat, prefix, GetActualMessage(), Date.Now.ToString()))
+			document.AppendText(String.Format(mesageFormat, prefix, GetActualMessage(), DateTime.Now.ToString()))
 
-            document.CaretPosition = document.Paragraphs.Get(document.Range.End).Range.Start
-            richEditControl1.ScrollToCaret()
-        End Sub
+			document.CaretPosition = document.Paragraphs.Get(document.Range.End).Range.Start
+			richEditControl1.ScrollToCaret()
+		End Sub
 
-        Private Function GetActualMessage() As String
-            Dim index As Integer = rnd.Next(3)
+		Private Function GetActualMessage() As String
+			Dim index As Integer = rnd.Next(3)
 
-            Select Case index
-                Case 0
-                    Return "Information"
-                Case 1
-                    Return "Warning"
-                Case 2
-                    Return "Error"
-                Case Else
-                    Return "Unknown"
-            End Select
-        End Function
+			Select Case index
+				Case 0
+					Return "Information"
+				Case 1
+					Return "Warning"
+				Case 2
+					Return "Error"
+				Case Else
+					Return "Unknown"
+			End Select
+		End Function
 
-        Private Sub ApplyDefaultDocumentFormat()
-            richEditControl1.Document.DefaultCharacterProperties.FontName = "Courier New"
-            richEditControl1.Document.DefaultCharacterProperties.FontSize = 12
-            richEditControl1.Document.DefaultCharacterProperties.Bold = True
-            richEditControl1.Document.DefaultCharacterProperties.ForeColor = Color.Blue
-            richEditControl1.Document.Sections(0).Page.Width = Units.InchesToDocumentsF(10F)
-        End Sub
-    End Class
+		Private Sub ApplyDefaultDocumentFormat()
+			richEditControl1.Document.DefaultCharacterProperties.FontName = "Courier New"
+			richEditControl1.Document.DefaultCharacterProperties.FontSize = 12
+			richEditControl1.Document.DefaultCharacterProperties.Bold = True
+			richEditControl1.Document.DefaultCharacterProperties.ForeColor = Color.Blue
+			richEditControl1.Document.Sections(0).Page.Width = Units.InchesToDocumentsF(10F)
+		End Sub
+	End Class
 End Namespace
--- VB/Program.vb
+++ VB/Program.vb
@@ -3,22 +3,18 @@ Imports System.Collections.Generic
 Imports System.Windows.Forms
 
 Namespace RichEditLogViewer
-    Friend NotInheritable Class Program
+	Friend Module Program
+		''' <summary>
+		''' The main entry point for the application.
+		''' </summary>
+		<STAThread>
+		Sub Main()
 
-        Private Sub New()
-        End Sub
+			DevExpress.LookAndFeel.UserLookAndFeel.Default.Style = DevExpress.LookAndFeel.LookAndFeelStyle.Style3D
 
-        ''' <summary>
-        ''' The main entry point for the application.
-        ''' </summary>
-        <STAThread> _
-        Shared Sub Main()
-
-            DevExpress.LookAndFeel.UserLookAndFeel.Default.Style = DevExpress.LookAndFeel.LookAndFeelStyle.Style3D
-
-            Application.EnableVisualStyles()
-            Application.SetCompatibleTextRenderingDefault(False)
-            Application.Run(New Form1())
-        End Sub
-    End Class
+			Application.EnableVisualStyles()
+			Application.SetCompatibleTextRenderingDefault(False)
+			Application.Run(New Form1())
+		End Sub
+	End Module
 End Namespace
