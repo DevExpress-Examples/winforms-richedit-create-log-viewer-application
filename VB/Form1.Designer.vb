b32e9f87cf8485d9c4106cc7aaaa3b3b30d11768
DevExpressExampleBot <fakeEmail>
2023-10-23 13:32:33 +0400

    Source auto update [skip ci]

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace RichEditLogViewer
-    Partial Public Class Form1
+Namespace RichEditLogViewer
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,14 +12,14 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
@@ -36,11 +38,14 @@
             Me.richEditControl1.Size = New System.Drawing.Size(810, 489)
             Me.richEditControl1.TabIndex = 0
             Me.richEditControl1.Text = "richEditControl1"
+            AddHandler Me.richEditControl1.EmptyDocumentCreated, New System.EventHandler(AddressOf Me.richEditControl1_EmptyDocumentCreated)
+            AddHandler Me.richEditControl1.DocumentLoaded, New System.EventHandler(AddressOf Me.richEditControl1_DocumentLoaded)
             ' 
             ' timer1
             ' 
             Me.timer1.Enabled = True
             Me.timer1.Interval = 1000
+            AddHandler Me.timer1.Tick, New System.EventHandler(AddressOf Me.timer1_Tick)
             ' 
             ' Form1
             ' 
@@ -51,13 +56,11 @@
             Me.Name = "Form1"
             Me.Text = "Form1"
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
+#End Region
+        Private richEditControl1 As DevExpress.XtraRichEdit.RichEditControl
 
-        Private WithEvents richEditControl1 As DevExpress.XtraRichEdit.RichEditControl
-        Private WithEvents timer1 As System.Windows.Forms.Timer
+        Private timer1 As System.Windows.Forms.Timer
     End Class
 End Namespace
-
